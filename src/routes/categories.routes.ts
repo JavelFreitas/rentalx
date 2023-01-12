01a9ee0394e8f8008077ee249314d76e62413f20
Javel Queiroz <javelqueiroz@outlook.com.br>
2023-01-12 00:17:45 -0300

    fix(category): create unique category by name rule

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,9 +7,14 @@ const categoryRepository = new CategoriesRepository();
 categoriesRoutes.post('', (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoryRepository.findByName(name);
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: 'Category already exists' });
+  }
+
   categoryRepository.create({ name, description });
 
-  response.status(201).json();
+  return response.status(201).json();
 });
 
 categoriesRoutes.get('/', (request, response) => {
